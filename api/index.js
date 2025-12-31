49ea1466632d37b50107f91a38e71c4fd97d1b5a
DevanshKhodaskar <devanshkhodaskar870@gmail.com>
2026-01-01 03:21:43 +0530

    Fix serverless function to include server build files

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import { createRequestHandler } from '@remix-run/node';
-import * as build from '../build/server/index.js';
 
-export default createRequestHandler({ build, mode: process.env.NODE_ENV });
+export default async function handler(req, res) {
+  // Dynamically import the server build
+  const build = await import('../build/server/index.js');
+
+  // Create the request handler
+  const remixHandler = createRequestHandler({
+    build,
+    mode: process.env.NODE_ENV,
+  });
+
+  return remixHandler(req, res);
+}
 
