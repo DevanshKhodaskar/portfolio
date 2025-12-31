ee3dc8a8ed4a68aaa46ac136c695bfcda87e240c
DevanshKhodaskar <devanshkhodaskar870@gmail.com>
2026-01-01 01:06:16 +0530

    fix: add wrangler for Remix dev cloudflare plugin

---
+++ remix.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  serverBuildDirectory: "build/server",
+  publicPath: "/build/",
+  serverModuleFormat: "esm",
+};
